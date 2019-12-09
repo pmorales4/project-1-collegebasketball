6c9e0d9bcba5c0d786974190af0307f468597a62
Phil Morales <pirs.hlpoae.iml@gmail.com>
2019-12-09 16:05:41 -0500

    finished with all modals for the conference logos

--- script.js
+++ script.js
@@ -62,18 +62,18 @@ function closeM2() {
 //THIS IS THE BIG TEN CONFERENCE
 
 const firstModal3 = document.querySelector("#firstModal3");
+console.log(firstModal3);
 let modalBtn3 = document.querySelector("#modalBtn3");
+console.log(modalBtn3);
 const closeFirstModal3 = document.querySelector(".closeFirstModal3");
-modalBtn3.addEventListener("click", openM3);
-closeFirstModal3.addEventListener("click", closeM3);
+console.log(closeFirstModal3);
+modalBtn3.addEventListener("click", function() {
+  firstModal3.style.display = "block";
+});
+closeFirstModal3.addEventListener("click", function() {
+  firstModal3.style.display = "none";
+});
 
-//FUNCTION BELOW FOR 3RD MODAL
-function openM3() {
-  firstModal2.style.display = "block";
-}
-function closeM3() {
-  firstModal2.style.display = "none";
-}
 //************* MODAL BIG TEN CONFERENCE ENDS ***********/
 
 //THIS IS THE BIG 12 CONFERENCE
@@ -84,9 +84,99 @@ modalBtn4.addEventListener("click", openM4);
 closeFirstModal4.addEventListener("click", closeM4);
 //FUNCTION BELOW FOR 3RD MODAL
 function openM4() {
-  firstModal2.style.display = "block";
+  firstModal4.style.display = "block";
 }
 function closeM4() {
-  firstModal2.style.display = "none";
+  firstModal4.style.display = "none";
 }
 //************* MODAL BIG 12 CONFERENCE ENDS ***********/
+
+//This is the Big East Conference
+const firstModal5 = document.querySelector("#firstModal5");
+let modalBtn5 = document.querySelector("#modalBtn5");
+const closeFirstModal5 = document.querySelector(".closeFirstModal5");
+modalBtn5.addEventListener("click", openM5);
+closeFirstModal5.addEventListener("click", closeM5);
+//FUNCTION BELOW FOR 3RD MODAL
+function openM5() {
+  firstModal5.style.display = "block";
+}
+function closeM5() {
+  firstModal5.style.display = "none";
+}
+//************* MODAL Big East CONFERENCE ENDS ***********/
+
+//This is the MEAC Conference
+const firstModal6 = document.querySelector("#firstModal6");
+let modalBtn6 = document.querySelector("#modalBtn6");
+const closeFirstModal6 = document.querySelector(".closeFirstModal6");
+modalBtn6.addEventListener("click", openM6);
+closeFirstModal6.addEventListener("click", closeM6);
+//FUNCTION BELOW FOR 3RD MODAL
+function openM6() {
+  firstModal6.style.display = "block";
+}
+function closeM6() {
+  firstModal6.style.display = "none";
+}
+//************* MODAL MEAC CONFERENCE ENDS ***********/
+
+//This is the Missouri Valleuy Conference
+const firstModal7 = document.querySelector("#firstModal7");
+let modalBtn7 = document.querySelector("#modalBtn7");
+const closeFirstModal7 = document.querySelector(".closeFirstModal7");
+modalBtn7.addEventListener("click", openM7);
+closeFirstModal7.addEventListener("click", closeM7);
+//FUNCTION BELOW FOR 3RD MODAL
+function openM7() {
+  firstModal7.style.display = "block";
+}
+function closeM7() {
+  firstModal7.style.display = "none";
+}
+//************* MODAL Missouri Valley CONFERENCE ENDS ***********/
+
+//This is the Ohio Valley Conference
+const firstModal8 = document.querySelector("#firstModal8");
+let modalBtn8 = document.querySelector("#modalBtn8");
+const closeFirstModal8 = document.querySelector(".closeFirstModal8");
+modalBtn8.addEventListener("click", openM8);
+closeFirstModal8.addEventListener("click", closeM8);
+//FUNCTION BELOW FOR 3RD MODAL
+function openM8() {
+  firstModal8.style.display = "block";
+}
+function closeM8() {
+  firstModal8.style.display = "none";
+}
+//************* MODAL Ohio Valley CONFERENCE ENDS ***********/
+
+//This is the Pac 12 Conference
+const firstModal9 = document.querySelector("#firstModal9");
+let modalBtn9 = document.querySelector("#modalBtn9");
+const closeFirstModal9 = document.querySelector(".closeFirstModal9");
+modalBtn9.addEventListener("click", openM9);
+closeFirstModal9.addEventListener("click", closeM9);
+//FUNCTION BELOW FOR 3RD MODAL
+function openM9() {
+  firstModal9.style.display = "block";
+}
+function closeM9() {
+  firstModal9.style.display = "none";
+}
+//************* MODAL Pac 12 CONFERENCE ENDS ***********/
+
+//This is the Pac 12 Conference
+const firstModal10 = document.querySelector("#firstModal10");
+let modalBtn10 = document.querySelector("#modalBtn10");
+const closeFirstModal10 = document.querySelector(".closeFirstModal10");
+modalBtn10.addEventListener("click", openM10);
+closeFirstModal10.addEventListener("click", closeM10);
+//FUNCTION BELOW FOR 3RD MODAL
+function openM10() {
+  firstModal10.style.display = "block";
+}
+function closeM10() {
+  firstModal10.style.display = "none";
+}
+//************* MODAL Pac 12 CONFERENCE ENDS ***********/
