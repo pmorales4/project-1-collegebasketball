a2bedbc44ec21942ca08549ed3ed806bd6e498d5
Phil Morales <pirs.hlpoae.iml@gmail.com>
2019-12-11 16:29:35 -0500

    both question and answer match randomly, make connection to buttons now

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -16,19 +16,82 @@ for(i = 0; i < 20; i++){
 if(prtAnswer == triviaGame[i].Correct){
   //console.log('they match');
 };
-  }
-console.log('------------------------------')
+}
+//console.log('------------------------------')
 
-console.log(triviaGame[1].Answer);
+//console.log(triviaGame[1].Answer);
+//console.log(triviaGame.length);
+for(i = 0; i < triviaGame.length; i++){
+  //console.log(triviaGame[i].Question);
+}
+//console.log('------------------------------')
+for(i = 0; i < triviaGame.length; i++){
+  //console.log(triviaGame[i].Answer);
+
+  //let firstAnswer = document.querySelector('#firstAnswer');
+
+  //console.log(triviaGame[i].Answer[0]);
+
+  //let randonIndex = Math.floor(Math.random() * triviaGame.length);
+  //firstAnswer.innerHTML = triviaGame[i].Answer[0];
+//console.log(firstAnswer);
+
+  // console.log(triviaGame[i].Answer[1]);
+  // console.log(triviaGame[i].Answer[2]);
+  // console.log(triviaGame[i].Answer[3]);
+}
+// console.log('------------------------------')
+// for(i = 0; i < triviaGame.length; i++){
+ // console.log(triviaGame[i].Correct);
+//}
+
+for(p = 0; p < triviaGame.length; p++){
+  let randonIndex = Math.floor(Math.random() * triviaGame.length);
+
+  questionDiv.innerHTML = triviaGame[randonIndex].Question;
+  //console.log(questionDiv);
 
 
-  // for(j = 0; j < 20; j++){
-  // let correctAnswer = triviaGame[j].Correct
-  // console.log(correctAnswer);
-  //   }
+   firstAnswer.innerHTML = triviaGame[randonIndex].Answer[0]
+   //console.log(firstAnswer);
+  // console.log(triviaGame[p].Answer[0]);
+//console.log(pop1);
 
+ secondAnswer.innerHTML = triviaGame[randonIndex].Answer[1]
+ //console.log(secondAnswer);
+  // console.log(triviaGame[p].Answer[1]);
+  //console.log(pop2);
 
-let a = triviaGame[19].Correct
+  thirdAnswer.innerHTML = triviaGame[randonIndex].Answer[2]
+  //console.log(thirdAnswer);
+  // console.log(triviaGame[p].Answer[2]);
+  //console.log(pop3);
+ 
+  forthAnswer.innerHTML = triviaGame[randonIndex].Answer[3]
+  //console.log(forthAnswer);
+  //console.log(triviaGame[p].Answer[3]);
+  //console.log(pop4);
+  //console.log('-------')
+}
+
+// for (x = 0; x < triviaGame.length; x++){
+//   let randonIndex = Math.floor(Math.random() * triviaGame.length);
+//   questionDiv.innerHTML = triviaGame[randonIndex].Question;
+// }
+
+let randonIndex = Math.floor(Math.random() * triviaGame.length);
+let answerElements = document.querySelector('.inputAnswer');
+
+//********************************************************* */
+//*************IMPORTANT LLOOP THROUGH WEEL..   */
+// for (u = 0; u < answerElements.length; u++){
+// questionDiv.innerHTML = triviaGame[randonIndex].Question;
+// answerElements[u].innerHTML = triviaGame[randonIndex].Answer[u];
+// //console.log(answerElements);
+// }
+//********************************** */
+
+//let a = triviaGame[19].Correct
 //console.log(a);
 
 //EXAMPLE BUTTON BELOW IN INDEX.HTML
@@ -40,17 +103,17 @@ answerBtn.setAttribute("align", "center");
 //document.body.appendChild(answerBtn).innerHTML = Answer[i];
 document.body.appendChild(answerBtn);
 
-let firstAnswer = document.querySelector('#firstAnswer');
-let secondAnswer = document.querySelector('#secondAnswer');
-let thirdAnswer = document.querySelector('#thirdAnswer');
-let forthAnswer = document.querySelector('#forthAnswer');
+//let firstAnswer = document.querySelector('#firstAnswer');
+//let secondAnswer = document.querySelector('#secondAnswer');
+//let thirdAnswer = document.querySelector('#thirdAnswer');
+//let forthAnswer = document.querySelector('#forthAnswer');
 // console.log(firstAnswer);
 // console.log(secondAnswer);
 // console.log(thirdAnswer);
 // console.log(forthAnswer);
 
-let randonIndex = Math.floor(Math.random() * triviaGame.length);
-  let randomAnswer = firstAnswer.innerHTML = triviaGame[randonIndex].Answer;
+//let randonIndex = Math.floor(Math.random() * triviaGame.length);
+  //let randomAnswer = firstAnswer.innerHTML = triviaGame[randonIndex].Answer;
   //console.log(randomAnswer);
 
 
@@ -62,13 +125,14 @@ let randonIndex = Math.floor(Math.random() * triviaGame.length);
 //document.body.appendChild(firstAnswer);
 //firstAnswer.innerHTML = triviaGame[randonIndex].Answer;
 //let randonIndex = Math.floor(Math.random() * triviaGame.length);
-let firstAnswer0 = firstAnswer.innerHTML = triviaGame[randonIndex].Answer;
+
+//let firstAnswer0 = firstAnswer.innerHTML = triviaGame[randonIndex].Answer;
 // console.log(firstAnswer0);
-let secondAnswer1 = secondAnswer.innerHTML = triviaGame[randonIndex].Answer;
+//let secondAnswer1 = secondAnswer.innerHTML = triviaGame[randonIndex].Answer;
 // console.log(secondAnswer1);
-let thirdAnswer2 = thirdAnswer.innerHTML = triviaGame[randonIndex].Answer;
+//let thirdAnswer2 = thirdAnswer.innerHTML = triviaGame[randonIndex].Answer;
 // console.log(thirdAnswer2);
-let forthAnswer3 = forthAnswer.innerHTML = triviaGame[randonIndex].Answer;
+//let forthAnswer3 = forthAnswer.innerHTML = triviaGame[randonIndex].Answer;
 // console.log(forthAnswer3);
 
 
@@ -140,17 +204,18 @@ let forthAnswer3 = forthAnswer.innerHTML = triviaGame[randonIndex].Answer;
 //document.getElementById("gameDiv").innerHTML = myOutput;
 //let x = JSON.stringify(document.getElementById("gameDiv").innerHTML = randomGame);
 //console.log(x);
+//************************************************************ */
+//IMPORTANT CODE BELOW
+// let nQuestion = document.querySelector("#nextBtn");
 
-let nQuestion = document.querySelector("#nextBtn");
-
-nQuestion.addEventListener("click", function() {
+// nQuestion.addEventListener("click", function() {
  
-  let randonIndex = Math.floor(Math.random() * triviaGame.length);
-  questionDiv.innerHTML = triviaGame[randonIndex].Question;
- answerDiv.innerHTML = triviaGame[randonIndex].Answer;
- // console.log(answerBtn.innerHTML = triviaGame[randonIndex].Correct);
-})
-
+//   let randonIndex = Math.floor(Math.random() * triviaGame.length);
+//   questionDiv.innerHTML = triviaGame[randonIndex].Question;
+//  answerDiv.innerHTML = triviaGame[randonIndex].Answer;
+//  // console.log(answerBtn.innerHTML = triviaGame[randonIndex].Correct);
+// })
+//*********************************************************************** */
 // let randonIndex = Math.floor(Math.random() * triviaGame.length);
 // let clickAnswer = answerDiv.innerHTML = triviaGame[randonIndex].Answer;
 
