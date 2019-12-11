7b766d9b708c91addb6fb2aa79ea1cdba71bbd2c
Phil Morales <pirs.hlpoae.iml@gmail.com>
2019-12-11 14:43:12 -0500

    some progress on logic

---
+++ js/gameArray.js
@@ -0,0 +1,184 @@
+//Array of objects for easy game
+let triviaGame = [
+  {
+    Question:
+      "What NCAA Division I basketball team has the most national championships?",
+    Correct: "UCLA",
+    Answer: ["UCLA", "Tar Heels", "Indiana", "Uconn"],
+  },
+  {
+    Question:
+      "Who received the First Naismith College Coach of the Year Award?",
+    Correct: "Coach Bobby Knight",
+    Answer: [
+      "Coach Bobby Knight",
+      "Coach Tom Moore",
+      "Coach Jim Boeheim",
+      "Coach Jim Calhoun",
+    ],
+  },
+  {
+    Question:
+      "The UCLA men hold the record for the most men's Division I NCAA chanpionships with 11. When did the UCLA men win their first basketball national championship?",
+    Correct: "The was 1964",
+    Answer: ["The was 1964", "It was 1965", "It was 1967", "It was 1989"],
+  },
+  {
+    Question: "When was the first college basketball game played?",
+    Correct: "January 18, 1896",
+    Answer: [
+      "January 18, 1896",
+      "January 21, 1895",
+      "February 14, 2013",
+      "February 20, 1896",
+    ],
+  },
+  {
+    Question:
+      " What school won the first NCAA Mens Basketball National Championships?",
+    Correct: "Oregon in 1939",
+    Answer: [
+      "Oregon in 1939",
+      "Uconn in 1950",
+      " Kentucky in 1999",
+      " Texas in 1943",
+    ],
+  },
+  {
+    Question: "What teams were in the Final Four back in 2000?",
+    Correct: "Tar Heels, Michigan State, Florida Gators, Wisconsin",
+    Answer: [
+      "Tar Heels, Michigan State, Florida Gators, Wisconsin",
+      "Uconn, Michigan State, Kentucky, Maryland -",
+      "Uconn, Maryland, UCLA, California -",
+      "Michigan, Duke, Baylor, Eastern Ct State University -",
+    ],
+  },
+  {
+    Question:
+      "What team won the National Championships in 2000 for Mens College Basketball",
+    Correct: "Michigan State Spartans",
+    Answer: [
+      "Michigan State Spartans",
+      "Kentucky Wildcats",
+      "Eastern Warriors",
+      "Golden State Warriors",
+    ],
+  },
+  {
+    Question:
+      "What team won the National Championship in 1995 mens college basketball?",
+    Correct: "UCLA Bruins",
+    Answer: ["UCLA Bruins", "Louisville", "Wisconsin", "Hawaii"],
+  },
+  {
+    Question:
+      "Who was the Most Outstanding Player in the 1995 Mens College Basketball Championship?",
+    Correct: "Ed O'Bannon - UCLA",
+    Answer: [
+      "Ed O'Bannon - UCLA",
+      "Rex Chapmen - Kentucky",
+      "Ray Allen - Uconn",
+      "Ronnie Seikaly - Syracuse",
+    ],
+  },
+  {
+    Question:
+      "The 1995 NBA draft picked two players from the North Carolina Tar Heels in the top 5, who are they?",
+    Correct: "Jerry Stackhouse and Rasheed Wallace",
+    Answer: [
+      "Jerry Stackhouse and Rasheed Wallace",
+      "Chris Webber and Jalen Rose",
+      "Scott Burrell and Ray Allen",
+      "Sherman Douglas and Derrick Coleman",
+    ],
+  },
+  {
+    Question:
+      "He was drafted out of Syracuse University in 1990 as the number pick in the NBA draft, who is he?",
+    Correct: "Derrick Coleman",
+    Answer: [
+      "Derrick Coleman",
+      "Billy Owens",
+      "Stephen Thompson",
+      "Sherman Douglas",
+    ],
+  },
+  {
+    Question:
+      "Syracuse University had a point guard that went by the name of the - General, who is he?",
+    Correct: "Sherman Douglas",
+    Answer: ["Sherman Douglas", "Conrad McRae", "Dave Johnson", "Mickey Mouse"],
+  },
+  {
+    Question:
+      "In 1984, Georgetown led by Patrick Ewing, won their first National Championship. Which school did they defeat 84-75 in the championship Game?",
+    Correct: "Houston - Phi Slamma Jamma",
+    Answer: [
+      "Houston - Phi Slamma Jamma",
+      "Georgie Tech",
+      "George Mason",
+      "University of Milwuakee",
+    ],
+  },
+  {
+    Question:
+      "In the 1992 East regional final, Christian Laettner hit the game winning basket from the top of the key with one second left to lift DUKE to a 104-103 win. What team was their opponent?",
+    Correct: "Kentucky Wildcats",
+    Answer: ["Kentucky Wildcats", "West Virginia", "Kansas", "Kansas State"],
+  },
+  {
+    Question: "Who was the lose against North Carolina in 1993?",
+    Correct: "East Caroline University",
+    Answer: ["East Caroline University", "Austin Peay", "Rutgers", "Purdue"],
+  },
+  {
+    Question:
+      "1985 was the first year the NCAA tournament had a grand total of 64 teams. Villanova defeated Georgetown to become the first national championship of the new format. Who was the loser?",
+    Correct: "Marshall",
+    Answer: [
+      "Marshall",
+      "Ohio State",
+      "North Carolina Central University",
+      "Howard University",
+    ],
+  },
+  {
+    Question:
+      "Kentucky was stunned by which all African American starting lineup team in the 1966 National Championship? Hint: a movie was made about this team. ",
+    Correct: "Texas Western University",
+    Answer: [
+      "Texas Western University",
+      "Alabama University",
+      "Mississippi State",
+      "Vanderbilt",
+    ],
+  },
+  {
+    Question:
+      "Syracuse was the National Champions in 2003, Who was the most outstanding polayer?",
+    Correct: "Carmelo Anthony",
+    Answer: [
+      "Carmelo Anthony",
+      "Nick Collison",
+      "Gerry McNamara",
+      "Kirk Hinrich",
+    ],
+  },
+  {
+    Question:
+      "The very first Final Four, played in 1939, was held in which midwestern city?",
+    Correct: "Evanston, Illinois",
+    Answer: [
+      "Evanston, Illinois",
+      "Waterbury, CT",
+      "Milwaukee, WI",
+      "Kansas City, MO",
+    ],
+  },
+  {
+    Question: "In 1993, Southern Univesity beat what #4 team?",
+    Correct: "Georgia Teach",
+    Answer: ["Georgia Teach", "Murray State", "St. Johns", "Butler University"],
+  },
+];
--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,139 +1,120 @@
-let questionDiv = document.createElement("div");
-let answerDiv = document.createElement("div");
+// let playerScore = 0;
 
-let questionDivStatement = document.createTextNode(
-  "this is a div for question"
-);
-let answerDivStatement = document.createTextNode("this is a div for answer");
+// first move this array into a new js file
+// load array.js before
+// gameLogic.js
+//DONE ***************************************************
 
-questionDiv.id = "questionDiv";
-questionDiv.setAttribute("align", "center");
-questionDiv.appendChild(questionDivStatement);
-document.body.appendChild(questionDiv);
+//function()
+// loop over Answer to then
+// document.createElement('button).innerHTML = `${answer[i]}`
 
-answerDiv.id = "answerDiv";
-answerDiv.setAttribute("align", "center");
-answerDiv.appendChild(answerDivStatement);
-document.body.appendChild(answerDiv);
+for(i = 0; i < 20; i++){
+  //THIS WORKS ***********************************
+ // let randonIndex = Math.floor(Math.random() * triviaGame.length);
+ let prtAnswer = triviaGame[i].Answer[0]
+if(prtAnswer == triviaGame[i].Correct){
+  //console.log('they match');
+};
+  }
+console.log('------------------------------')
 
-let score = 0;
+console.log(triviaGame[1].Answer);
 
-//Array of objects for easy game
-let triviaGame = [
-  {
-    Question:
-      "What NCAA Division I basketball team has the most national championships?",
-    Answer: ["UCLA", "Tar Heels", "Indiana", "Uconn"],
-  },
-  {
-    Question:
-      "Who received the First Naismith College Coach of the Year Award?",
-    Answer: [
-      "Coach Bobby Knight",
-      "Coach Tom Moore",
-      "Coach Jim Boeheim",
-      "Coach Jim Calhoun",
-    ],
-  },
-  {
-    Question:
-      "The UCLA men hold the record for the most men's Division I NCAA chanpionships with 11. When did the UCLA men win their first basketball national championship?",
-    Answer: "The was 1964",
-  },
-  {
-    Question: "When was the first college basketball game played?",
-    Answer: "January 18, 1896",
-  },
-  {
-    Question:
-      " What school won the first NCAA Mens Basketball National Championships?",
-    Answer: "Oregon in 1939",
-  },
-  {
-    Question: "What teams were in the Final Four back in 2000?",
-    Answer: "Tar Heels, Michigan State, Florida Gators, Wisconsin",
-  },
-  {
-    Question:
-      "What team won the National Championships in 2000 for Mens College Basketball",
-    Answer: "Michigan State Spartans",
-  },
-  {
-    Question:
-      "What team won the National Championship in 1995 mens college basketball?",
-    Answer: "UCLA Bruins",
-  },
-  {
-    Question:
-      "Who was the Most Outstanding Player in the 1995 Mens College Basketball Championship?",
-    Answer: "Ed O'Bannon - UCLA",
-  },
-  {
-    Question:
-      "The 1995 NBA draft picked two players from the North Carolina Tar Heels in the top 5, who are they?",
-    Answer: "Jerry Stackhouse and Rasheed Wallace",
-  },
-  {
-    Question:
-      "He was drafted out of Syracuse University in 1990 as the number pick in the NBA draft, who is he?",
-    Answer: "Derrick Coleman",
-  },
-  {
-    Question:
-      "Syracuse University had a point guard that went by the name of the - General, who is he?",
-    Answer: "Sherman Douglas",
-  },
-  {
-    Question:
-      "In 1984, Georgetown led by Patrick Ewing, won their first National Championship. Which school did they defeat 84-75 in the championship Game?",
-    Answer: "Houston - Phi Slamma Jamma",
-  },
-  {
-    Question:
-      "In the 1992 East regional final, Christian Laettner hit the game winning basket from the top of the key with one second left to lift DUKE to a 104-103 win. What team was their opponent?",
-    Answer: "Kentucky Wildcats",
-  },
-  {
-    Question: "Who was the loset against North Carolina in 1993?",
-    Answer: "East Caroline University",
-  },
-  {
-    Question:
-      "1985 was the first year the NCAA tournament had a grand total of 64 teams. Villanova defeated Georgetown to become the first national championship of the new format. Who was the loser?",
-    Answer: "Marshall",
-  },
-  {
-    Question:
-      "Kentucky was stunned by which all African American starting lineup team in the 1966 National Championship? Hint: a movie was made about this team. ",
-    Answer: "Texas Western University",
-  },
-  {
-    Question:
-      "Syracuse was the National Champions in 2003, Who was the most outstanding polayer?",
-    Answer: "Carmelo Anthony",
-  },
-  {
-    Question:
-      "The very first Final Four, played in 1939, was held in which midwestern city?",
-    Answer: "Evanston, Illinois",
-  },
-  {
-    Question: "In 1993, Southern Univesity beat what #4 team?",
-    Answer: "Georgia Teach",
-  },
-];
 
-for (let j = 0; j < triviaGame.length; j++) {
-  //console.log(triviaGame[j]);
-}
+  // for(j = 0; j < 20; j++){
+  // let correctAnswer = triviaGame[j].Correct
+  // console.log(correctAnswer);
+  //   }
+
+
+let a = triviaGame[19].Correct
+//console.log(a);
+
+//EXAMPLE BUTTON BELOW IN INDEX.HTML
+
+let answerBtn = document.createElement("button");
+answerBtn.id = "answerBtn";
+answerBtn.setAttribute("align", "center");
+//answerBtn.appendChild(easyGameBtn);
+//document.body.appendChild(answerBtn).innerHTML = Answer[i];
+document.body.appendChild(answerBtn);
+
+let firstAnswer = document.querySelector('#firstAnswer');
+let secondAnswer = document.querySelector('#secondAnswer');
+let thirdAnswer = document.querySelector('#thirdAnswer');
+let forthAnswer = document.querySelector('#forthAnswer');
+// console.log(firstAnswer);
+// console.log(secondAnswer);
+// console.log(thirdAnswer);
+// console.log(forthAnswer);
+
+let randonIndex = Math.floor(Math.random() * triviaGame.length);
+  let randomAnswer = firstAnswer.innerHTML = triviaGame[randonIndex].Answer;
+  //console.log(randomAnswer);
+
+
+
+
+
+
+//firstAnswer.setAttribute('align', "center");
+//document.body.appendChild(firstAnswer);
+//firstAnswer.innerHTML = triviaGame[randonIndex].Answer;
+//let randonIndex = Math.floor(Math.random() * triviaGame.length);
+let firstAnswer0 = firstAnswer.innerHTML = triviaGame[randonIndex].Answer;
+// console.log(firstAnswer0);
+let secondAnswer1 = secondAnswer.innerHTML = triviaGame[randonIndex].Answer;
+// console.log(secondAnswer1);
+let thirdAnswer2 = thirdAnswer.innerHTML = triviaGame[randonIndex].Answer;
+// console.log(thirdAnswer2);
+let forthAnswer3 = forthAnswer.innerHTML = triviaGame[randonIndex].Answer;
+// console.log(forthAnswer3);
+
+
+  // let randonIndex = Math.floor(Math.random() * triviaGame.length);
+  // answerDiv.innerHTML = triviaGame[randonIndex].Answer;
+
+
+// checkingLogic()
+// userclick = chosenAnswer
+// chosenAnswer == obj.correct => score()
+// if user is wrong => nextQuestion() don't add to score
+
+// timer()
+//setTimeout() REASEARCH THIS
+// .innerHTML = i
+
+// score()
+// 0 and then +1 everytime the user is right
+
+//
+// {
+//   Question: "Who received the First Naismith College Coach of the Year Award?",
+//   Correct: "Coach Bobby Knight",
+//   Answer: [
+//     "Coach Bobby Knight",
+//     "Coach Tom Moore",
+//     "Coach Jim Boeheim",
+//     "Coach Jim Calhoun",
+//   ],
+// },
+
+//
+
+//********************************************************* */
+
+//for (let j = 0; j < triviaGame.length; j++) {
+//console.log(triviaGame[j]);
+//
 
 //how to grab one item in array
-let b = triviaGame[1].Question;
-let c = triviaGame[0].Answer;
+// let b = triviaGame[1].Question;
+// let c = triviaGame[0].Answer;
 //console.log(triviaGame[16].Answer17); // this grabs all separate answer the way its written.
 //document.getElementById("gameDiv").innerHTML = b;
-console.log(b);
-console.log(c);
+// console.log(b);
+// console.log(c);
 
 //create random number
 //for (i = 0; i < triviaGame.length; i++) {
@@ -163,10 +144,19 @@ console.log(c);
 let nQuestion = document.querySelector("#nextBtn");
 
 nQuestion.addEventListener("click", function() {
+ 
   let randonIndex = Math.floor(Math.random() * triviaGame.length);
   questionDiv.innerHTML = triviaGame[randonIndex].Question;
-  answerDiv.innerHTML = triviaGame[randonIndex].Answer;
-});
+ answerDiv.innerHTML = triviaGame[randonIndex].Answer;
+ // console.log(answerBtn.innerHTML = triviaGame[randonIndex].Correct);
+})
+
+// let randonIndex = Math.floor(Math.random() * triviaGame.length);
+// let clickAnswer = answerDiv.innerHTML = triviaGame[randonIndex].Answer;
+
+
+
+
 
 //3 lines below work well. ***********************************************
 // let randonIndex = Math.floor(Math.random() * triviaGame.length);
