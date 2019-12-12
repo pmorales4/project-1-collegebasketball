ad6d76490e6964f852337a751c651950b71ed385
Phil Morales <pirs.hlpoae.iml@gmail.com>
2019-12-12 16:43:12 -0500

    some corrections

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -4,174 +4,151 @@ secondAnswer.innerHTML = triviaGame[0].Answer[1];
 thirdAnswer.innerHTML = triviaGame[0].Answer[2];
 forthAnswer.innerHTML = triviaGame[0].Answer[3];
 
-//function()
-// loop over Answer to then
-// document.createElement('button).innerHTML = `${answer[i]}`
-
 // ***************************************************************
 // THIS EVENT LISTENER POPULATES THE QUESTION AND ANSWER TO SCREEN
 // ***************************************************************
 let finalScore = 0;
-
 let maxScore = 20;
-
+let randomIndex = 0;
 let nQuestion = document.querySelector("#nextBtn");
+let resetBtn = document.querySelector("#resetBtn");
+let exitBtn = document.querySelector("#exitBtn");
+
+//randomIndex = Math.floor(Math.random() * triviaGame.length);
+
+
+
 
 nQuestion.addEventListener("click", function() {
-  //for (p = 0; p < triviaGame.length; p++) {
   //GET THE RANDOM ITEM FROM ARRAY OF OBJECTS
-  let randonIndex = Math.floor(Math.random() * triviaGame.length);
+  randomIndex = Math.floor(Math.random() * triviaGame.length);
   // console.log(randonIndex);
-  questionDiv.innerHTML = triviaGame[randonIndex].Question;
-  // console.log(questionDiv);
-  // let firstAnswer = document.querySelector("#firstAnswer");
-  // let secondAnswer = document.querySelector("#secondAnswer");
-  // let thirdAnswer = document.querySelector("#thirdAnswer");
-  // let forthAnswer = document.querySelector("#forthAnswer");
-  firstAnswer.innerHTML = triviaGame[randonIndex].Answer[0];
-  // console.log(firstAnswer);
-  secondAnswer.innerHTML = triviaGame[randonIndex].Answer[1];
-  // console.log(secondAnswer);
-  thirdAnswer.innerHTML = triviaGame[randonIndex].Answer[2];
-  // console.log(thirdAnswer);
-  forthAnswer.innerHTML = triviaGame[randonIndex].Answer[3];
-  // console.log(forthAnswer);
-  //}
+  questionDiv.innerHTML = triviaGame[randomIndex].Question;
+  firstAnswer.innerHTML = triviaGame[randomIndex].Answer[0];
+  secondAnswer.innerHTML = triviaGame[randomIndex].Answer[1];
+  thirdAnswer.innerHTML = triviaGame[randomIndex].Answer[2];
+  forthAnswer.innerHTML = triviaGame[randomIndex].Answer[3];
 });
 
 //***************************************************************
 //***************************************************************
 
+//FUNCTION MAKE A NEW SCREEN OF QUESTION AND ANSWERS
 function nextQuestion() {
-  let randonIndex = Math.floor(Math.random() * triviaGame.length);
-  questionDiv.innerHTML = triviaGame[randonIndex].Question;
-  firstAnswer.innerHTML = triviaGame[randonIndex].Answer[0];
-  secondAnswer.innerHTML = triviaGame[randonIndex].Answer[1];
-  thirdAnswer.innerHTML = triviaGame[randonIndex].Answer[2];
-  forthAnswer.innerHTML = triviaGame[randonIndex].Answer[3];
+  randomIndex = Math.floor(Math.random() * triviaGame.length);
+  questionDiv.innerHTML = triviaGame[randomIndex].Question;
+  firstAnswer.innerHTML = triviaGame[randomIndex].Answer[0];
+  secondAnswer.innerHTML = triviaGame[randomIndex].Answer[1];
+  thirdAnswer.innerHTML = triviaGame[randomIndex].Answer[2];
+  forthAnswer.innerHTML = triviaGame[randomIndex].Answer[3];
 }
 
-let answerBtn = document.querySelector("span");
-console.log(answerBtn);
-
-firstAnswer.addEventListener("click", function() {
-  let randonIndex = Math.floor(Math.random() * triviaGame.length);
-  //questionDiv.innerHTML = triviaGame[0].Question;
-  for (i = 0; i < triviaGame.length; i++) {
-   // console.log(triviaGame[i]);
-   let pp = triviaGame[randonIndex].Answer[0] == triviaGame[i].Correct; 
-console.log(pp);
-if (pp){
-  if(pp = true){
-    finalScore = finalScore + 1;
-    nextQuestion();
-  } else if (pp = false) {
-    nextQuestion();
-  }
-  
-//   console.log("right answer");
-//       finalScore = finalScore + 1;
-//       nextQuestion();
-// } else if 
-//   (triviaGame[randonIndex].Answer[0] !== triviaGame[randonIndex].Correct) {
-//       console.log("wrong answer");
-//       nextQuestion();
-// }
-
-    // //finalScore = finalScore + 1;
-    // if (finalScore == maxScore) {
-    //   console.log(finalScore);
-
-    //   //}
-      answerBtn.innerHTML = finalScore;
-    }
-  }
-});
-
-// secondAnswer.addEventListener("click", function() {
-//   let randonIndex = Math.floor(Math.random() * triviaGame.length);
-//   for (i = 0; i < triviaGame.length; i++) {
-//     if (triviaGame[randonIndex].Answer[1] == triviaGame[i].Correct) {
-//       console.log("right answer");
-//       finalScore = finalScore + 1;
-//       nextQuestion();
-//     } else if (triviaGame[randonIndex].Answer[1] !== triviaGame[i].Correct) {
-//       console.log("wrong answer");
-//       nextQuestion();
-//     }
-
-//     // finalScore = finalScore + 1;
-//     if (finalScore == maxScore) {
-//       console.log(finalScore);
-//       answerBtn.innerHTML = finalScore;
-//     }
-//   }
-// });
-
-// thirdAnswer.addEventListener("click", function() {
-//   let randonIndex = Math.floor(Math.random() * triviaGame.length);
-//   for (i = 0; i < triviaGame.length; i++) {
-//     if (triviaGame[randonIndex].Answer[2] == triviaGame[i].Correct) {
-//       console.log("right answer");
-//       finalScore = finalScore + 1;
-//       nextQuestion();
-//     } else if (triviaGame[randonIndex].Answer[2] !== triviaGame[i].Correct) {
-//       console.log("wrong answer");
-//       nextQuestion();
-//     }
-
-//     if (finalScore == maxScore) {
-//       //finalScore = finalScore + 1;
-//       console.log(finalScore);
-//       answerBtn.innerHTML = finalScore;
-//     }
-//   }
-// });
-
-// forthAnswer.addEventListener("click", function() {
-//   let randonIndex = Math.floor(Math.random() * triviaGame.length);
-//   for (i = 0; i < triviaGame.length; i++) {
-//     if (triviaGame[randonIndex].Answer[3] == triviaGame[i].Correct) {
-//       console.log("right answer");
-//       finalScore = finalScore + 1;
-//       nextQuestion();
-//     } else if (triviaGame[randonIndex].Answer[3] !== triviaGame[i].Correct) {
-//       console.log("wrong answer");
-//       nextQuestion();
-//     }
-
-//     if (finalScore == maxScore) {
-//       console.log(finalScore);
-//       answerBtn.innerHTML = finalScore;
-//     }
-//   }
-// });
-
+function reachTwenty() {
+  alert("YOU WON THE GAME!!!!!!!!!!!!!");
+}
 //RESET BUTTON WORKS, REFRESHES THE WHOLE PAGE.
-let resetBtn = document.querySelector("#resetBtn");
 
 resetBtn.addEventListener("click", function() {
   location.reload();
 });
 
 //exit button not working.
-let exitBtn = document.querySelector("#exitBtn");
+
 exitBtn.addEventListener("click", function() {
   console.log("pressed exit, does not work");
 });
 
-// score()
-// 0 and then +1 everytime the user is right
-//
-// {
-//   Question: "Who received the First Naismith College Coach of the Year Award?",
-//   Correct: "Coach Bobby Knight",
-//   Answer: [
-//     "Coach Bobby Knight",
-//     "Coach Tom Moore",
-//     "Coach Jim Boeheim",
-//     "Coach Jim Calhoun",
-//   ],
-// },
+//******************************************* */
 
-//
+let answerBtn = document.querySelector("span");
+console.log(answerBtn);
+
+//FIRST EVENT LISTENER BUTTON - CODE BELOW.
+//RANDOMLY SELECTS QUESTION AND FINDS ANSWERS
+//ONCE FOUND - LOGIC IS EITHER ANSWER IS TRUE FOR RIGHT
+//FALSE FOR WRONG
+//IF TRUE SCORE INCREMENTS BY 1
+//GOERS TO NEXT QUESTION.
+firstAnswer.addEventListener("click", function() {
+ 
+    let pp = triviaGame[randomIndex].Answer[0] == triviaGame[randomIndex].Correct;
+    console.log(pp);
+    
+      if ((pp === true)) {
+        finalScore = finalScore + 1;
+        nextQuestion();
+        answerBtn.innerHTML = finalScore;
+        if (finalScore == maxScore) {
+          reachTwenty();
+        }
+      } else (pp === false) 
+        nextQuestion();
+      
+
+    
+    
+  
+ 
+});
+
+secondAnswer.addEventListener("click", function() {
+  
+ 
+    let pp = triviaGame[randomIndex].Answer[1] == triviaGame[randomIndex].Correct;
+    console.log(pp);
+  
+      if ((pp === true)) {
+        finalScore = finalScore + 1;
+        nextQuestion();
+        answerBtn.innerHTML = finalScore;
+        if (finalScore == maxScore) {
+          reachTwenty();
+        }
+      } else pp === false;
+      nextQuestion();
+    
+     
+    
+ 
+});
+
+thirdAnswer.addEventListener("click", function() {
+
+  let pp = triviaGame[randomIndex].Answer[2] == triviaGame[randomIndex].Correct;
+  console.log(pp);
+
+    if ((pp === true)) {
+      finalScore = finalScore + 1;
+      nextQuestion();
+      answerBtn.innerHTML = finalScore;
+      if (finalScore == maxScore) {
+        reachTwenty();
+      }
+    } else pp === false;
+    nextQuestion();
+  
+    
+});
+
+forthAnswer.addEventListener("click", function() {
+  let pp = triviaGame[randomIndex].Answer[3] == triviaGame[randomIndex].Correct;
+  console.log(pp);
+
+    if ((pp === true)) {
+      finalScore = finalScore + 1;
+      nextQuestion();
+      answerBtn.innerHTML = finalScore;
+      if (finalScore == maxScore) {
+        reachTwenty();
+      }
+    } else pp === false;
+    nextQuestion();
+  
+  
+});
+
+
+
+
+
+
