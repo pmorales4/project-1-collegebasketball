dccae051f581fe6403e2145871fb4cfb9407fe33
Phil Morales <pirs.hlpoae.iml@gmail.com>
2019-12-12 12:10:24 -0500

    some functionality on the button answers fixed, still not working correctly, reset button fixed, score does not add correctly.

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,14 +1,8 @@
-
 questionDiv.innerHTML = triviaGame[0].Question;
 firstAnswer.innerHTML = triviaGame[0].Answer[0];
 secondAnswer.innerHTML = triviaGame[0].Answer[1];
 thirdAnswer.innerHTML = triviaGame[0].Answer[2];
 forthAnswer.innerHTML = triviaGame[0].Answer[3];
-
-
-
-
-
 
 //function()
 // loop over Answer to then
@@ -23,129 +17,162 @@ let maxScore = 20;
 let nQuestion = document.querySelector("#nextBtn");
 
 nQuestion.addEventListener("click", function() {
-  for (p = 0; p < triviaGame.length; p++) {
-    //GET THE RANDOM ITEM FROM ARRAY OF OBJECTS
-    let randonIndex = Math.floor(Math.random() * triviaGame.length);
-    questionDiv.innerHTML = triviaGame[randonIndex].Question;
-    // let firstAnswer = document.querySelector("#firstAnswer");
-    // let secondAnswer = document.querySelector("#secondAnswer");
-    // let thirdAnswer = document.querySelector("#thirdAnswer");
-    // let forthAnswer = document.querySelector("#forthAnswer");
-    firstAnswer.innerHTML = triviaGame[randonIndex].Answer[0];
-    secondAnswer.innerHTML = triviaGame[randonIndex].Answer[1];
-    thirdAnswer.innerHTML = triviaGame[randonIndex].Answer[2];
-    forthAnswer.innerHTML = triviaGame[randonIndex].Answer[3];
-  }
+  //for (p = 0; p < triviaGame.length; p++) {
+  //GET THE RANDOM ITEM FROM ARRAY OF OBJECTS
+  let randonIndex = Math.floor(Math.random() * triviaGame.length);
+  // console.log(randonIndex);
+  questionDiv.innerHTML = triviaGame[randonIndex].Question;
+  // console.log(questionDiv);
+  // let firstAnswer = document.querySelector("#firstAnswer");
+  // let secondAnswer = document.querySelector("#secondAnswer");
+  // let thirdAnswer = document.querySelector("#thirdAnswer");
+  // let forthAnswer = document.querySelector("#forthAnswer");
+  firstAnswer.innerHTML = triviaGame[randonIndex].Answer[0];
+  // console.log(firstAnswer);
+  secondAnswer.innerHTML = triviaGame[randonIndex].Answer[1];
+  // console.log(secondAnswer);
+  thirdAnswer.innerHTML = triviaGame[randonIndex].Answer[2];
+  // console.log(thirdAnswer);
+  forthAnswer.innerHTML = triviaGame[randonIndex].Answer[3];
+  // console.log(forthAnswer);
+  //}
 });
 
 //***************************************************************
 //***************************************************************
+
+function nextQuestion() {
+  let randonIndex = Math.floor(Math.random() * triviaGame.length);
+  questionDiv.innerHTML = triviaGame[randonIndex].Question;
+  firstAnswer.innerHTML = triviaGame[randonIndex].Answer[0];
+  secondAnswer.innerHTML = triviaGame[randonIndex].Answer[1];
+  thirdAnswer.innerHTML = triviaGame[randonIndex].Answer[2];
+  forthAnswer.innerHTML = triviaGame[randonIndex].Answer[3];
+}
 
 let answerBtn = document.querySelector("span");
 console.log(answerBtn);
 
+// firstAnswer.addEventListener("click", function() {
+//   let randonIndex = Math.floor(Math.random() * triviaGame.length);
+//   //questionDiv.innerHTML = triviaGame[0].Question;
+//   for (i = 0; i < triviaGame.length; i++) {
+//   if (triviaGame[randonIndex].Answer[0] == triviaGame[i].Correct) {
+//     console.log("right answer");
 
-firstAnswer.addEventListener("click", function() {
-  let randonIndex = Math.floor(Math.random() * triviaGame.length);
-  //questionDiv.innerHTML = triviaGame[0].Question;
-  for (i = 0; i < triviaGame.length; i++) {
-  if (triviaGame[randonIndex].Answer[0] == triviaGame[i].Correct) {
-    console.log("you got the right answer on index 0");
-    
-    finalScore = finalScore + 1;
-    if (finalScore == 20){
-    console.log(finalScore);
-    alert('YOU WON GAME *************');
-  }
-    answerBtn.innerHTML = finalScore;
-  }
-}
-});
+//     finalScore = finalScore + 1;
+//     if (finalScore == 20){
+//     console.log(finalScore);
+//     alert('YOU WON GAME *************');
+//   }
+//     answerBtn.innerHTML = finalScore;
+//   }
+// }
+// });
 
 secondAnswer.addEventListener("click", function() {
   let randonIndex = Math.floor(Math.random() * triviaGame.length);
   for (i = 0; i < triviaGame.length; i++) {
-  if (triviaGame[randonIndex].Answer[1] == triviaGame[i].Correct) {
-    console.log("you got the right answer on index 1 ");
-    finalScore = finalScore + 1;
-    console.log(finalScore);
-    answerBtn.innerHTML = finalScore;
+    if (triviaGame[i].Answer[1] == triviaGame[i].Correct) {
+      console.log("right answer");
+      finalScore = finalScore + 1;
+      nextQuestion();
+    } else if (triviaGame[i].Answer[1] !== triviaGame[i].Correct) {
+      console.log("wrong answer");
+      nextQuestion();
+    }
+
+    // finalScore = finalScore + 1;
+    if (finalScore == maxScore) {
+      console.log(finalScore);
+      answerBtn.innerHTML = finalScore;
+    }
   }
-}
 });
 
 thirdAnswer.addEventListener("click", function() {
   let randonIndex = Math.floor(Math.random() * triviaGame.length);
   for (i = 0; i < triviaGame.length; i++) {
-  if (triviaGame[randonIndex].Answer[2] == triviaGame[i].Correct) {
-    console.log("you got the right answer on index 2");
-    finalScore = finalScore + 1;
-    console.log(finalScore);
-    answerBtn.innerHTML = finalScore;
+    if (triviaGame[i].Answer[2] == triviaGame[i].Correct) {
+      console.log("right answer");
+      finalScore = finalScore + 1;
+      nextQuestion();
+    } else if (triviaGame[i].Answer[2] !== triviaGame[i].Correct) {
+      console.log("wrong answer");
+      nextQuestion();
+    }
+
+    if (finalScore == maxScore) {
+      //finalScore = finalScore + 1;
+      console.log(finalScore);
+      answerBtn.innerHTML = finalScore;
+    }
   }
-}
 });
 
 forthAnswer.addEventListener("click", function() {
   let randonIndex = Math.floor(Math.random() * triviaGame.length);
   for (i = 0; i < triviaGame.length; i++) {
-  if (triviaGame[randonIndex].Answer[3] == triviaGame[i].Correct) {
-    console.log("you got the right answer on index 3");
-    finalScore = finalScore + 1;
-    console.log(finalScore);
-    answerBtn.innerHTML = finalScore;
+    if (triviaGame[i].Answer[3] == triviaGame[i].Correct) {
+      console.log("right answer");
+      finalScore = finalScore + 1;
+      nextQuestion();
+    } else if (triviaGame[i].Answer[3] !== triviaGame[i].Correct) {
+      console.log("wrong answer");
+      nextQuestion();
+    }
+
+    if (finalScore == maxScore) {
+      console.log(finalScore);
+      answerBtn.innerHTML = finalScore;
+    }
   }
-}
 });
 
+firstAnswer.addEventListener("click", function() {
+  let randonIndex = Math.floor(Math.random() * triviaGame.length);
+  //questionDiv.innerHTML = triviaGame[0].Question;
+  for (i = 0; i < triviaGame.length; i++) {
+    console.log(triviaGame[i]);
+    if (triviaGame[i].Answer[0] == triviaGame[i].Correct) {
+      console.log("right answer");
+      finalScore = finalScore + 1;
+      nextQuestion();
+    } else if (triviaGame[i].Answer[0] !== triviaGame[i].Correct) {
+      console.log("wrong answer");
+      nextQuestion();
+    }
 
-//let x = triviaGame[randonIndex].Answer
-//console.log(x);
-//x[Math.floor(Math.random()) * triviaGame.Answer];
-//console.log(x);
-//x.forEach(element => console.log(element));
-//______________________________________________________________
-//let randonIndex = Math.floor(Math.random() * triviaGame.length);
-// let el = Math.floor(Math.random(x) * triviaGame[0].Answer.length);
-// if(el == 0){
-//   console.log(triviaGame[randonIndex].Answer[0])
-// } else if (el == 1){
-//   console.log(triviaGame[randonIndex].Answer[1])
-// } else if (el == 2){
-//   console.log(triviaGame[randonIndex].Answer[2])
-// } else {
-//   console.log(triviaGame[randonIndex].Answer[3])
-// }
-// console.log(el);
-//________________________________________________________
+    //finalScore = finalScore + 1;
+    if (finalScore == maxScore) {
+      console.log(finalScore);
 
+      //}
+      answerBtn.innerHTML = finalScore;
+    }
+  }
+});
 
-//********************************************************* */
-//*************IMPORTANT LLOOP THROUGH WEEL..   */
-// for (u = 0; u < answerElements.length; u++){
-// questionDiv.innerHTML = triviaGame[randonIndex].Question;
-// answerElements[u].innerHTML = triviaGame[randonIndex].Answer[u];
-// //console.log(answerElements);
-// }
-//********************************** */
+//RESET BUTTON WORKS, REFRESHES THE WHOLE PAGE.
+let resetBtn = document.querySelector("#resetBtn");
 
+resetBtn.addEventListener("click", function() {
+  location.reload();
+});
 
+let exitBtn = document.querySelector("#exitBtn");
+exitBtn.addEventListener("click", function() {
+  exit();
+});
 
 // checkingLogic()
 // userclick = chosenAnswer
 // chosenAnswer == obj.correct => score()
 // if user is wrong => nextQuestion() don't add to score
 
-
-
-
-
 // timer()
 //setTimeout() REASEARCH THIS
 // .innerHTML = i
-
-
-
 
 // score()
 // 0 and then +1 everytime the user is right
